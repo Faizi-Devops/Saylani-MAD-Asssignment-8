954a177baf24d88573dfcbaebcaf2bc7c61bfb39
Faizan Ali <devfaizi435@gmail.com>
2022-10-10 02:10:50 -0700

    11) program to count frequency of each element in an array

--- 11) program to count frequency of each element in an array/script.js
+++ 11) program to count frequency of each element in an array/script.js
@@ -1,16 +1,17 @@
 var count;
-var nusy;
+var num;
+var c = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var Hello = function (b) {
-    for (var i = 0; i <= 100; i++) {
+    for (var i = 0; i <= c.length - 1; i++) {
         count = 0;
         for (var j = 0; j <= b.length - 1; j++) {
-            nusy = b[j];
-            if (nusy == i) {
+            if (c[i] == b[j]) {
                 count = count + 1;
-                console.log(nusy);
+                num = c[i];
             }
         }
+        console.log("".concat(num, " has ").concat(count, " times"));
     }
 };
-var a = [1, 2, 1, 5, 7, 30, 50, 30, 50, 50, 40];
+var a = [1, 2, 5, 8, 3, 8, 3, 5, 7, 3, 6, 8, 3, 5, 7, 1];
 Hello(a);
--- 11) program to count frequency of each element in an array/script.ts
+++ 11) program to count frequency of each element in an array/script.ts
@@ -1,25 +1,23 @@
 let count:number;
-let nusy:number;
+let num:number;
+let c:number[]=[1,2,3,4,5,6,7,8,9];
 const Hello = (b:number[]) =>{
-    for(let i :number=0;i<=100;i++){
+    for(let i:number=0;i<=c.length-1;i++){
         count=0;
         for(let j:number=0;j<=b.length-1;j++){
-            nusy=b[j];
-            if(nusy==i){
-                count = count + 1;
-                console.log(nusy)
+            if(c[i]==b[j]){
+                count = count+1;
+                num=c[i]
+
             }
         }
+        console.log(`${num} has ${count} times`)
         
-        
-
     }
-    
 
 
 
 
 }
-let a:number[]=[1,2,1,5,7,30,50,30,50,50,40];
-
-Hello(a)
+let a:number[]=[1,2,5,8,3,8,3,5,7,3,6,8,3,5,7,1];
+Hello(a);
